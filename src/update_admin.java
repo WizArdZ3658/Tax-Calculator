4ecd9bacef557e20c74eaaedadcfa841e45bf3c6
Somnath <p.somnath2599@gmail.com>
2019-08-21 23:14:25 +0530

    Initial commit

---
+++ src/update_admin.java
@@ -0,0 +1,468 @@
+import java.awt.event.*;
+import javax.swing.*;
+import java.lang.*;
+import java.sql.*;
+import java.awt.*;
+import java.util.Random;
+
+import BCrypt.*;
+
+public class update_admin extends JPanel implements ActionListener {
+
+    private JLabel l[];
+    private JLabel l2[];
+    private JLabel l3;
+    private JTextField tf[];
+    private JPasswordField pf1, pf2;
+    private JRadioButton rb[];
+    private ButtonGroup bg1;
+    private JButton b1, b2, b3;
+    private String username ;
+    private masterwindow_admin pointer;
+    private String old_email_id;
+
+    public update_admin(masterwindow_admin parent, String uid) {
+        username = uid;
+        pointer = parent;
+        l = new JLabel[10];
+        l2 = new JLabel[10];
+        int y2 = 50;
+        for (int i = 0; i < 10; i++) {
+            l2[i] = new JLabel("*");
+            l2[i].setBounds(610, y2, 15, 35);
+            y2 += 50;
+            l2[i].setVisible(false);
+            add(l2[i]);
+        }
+        tf = new JTextField[7];
+        for (int i = 0; i < tf.length; i++) {
+            tf[i] = new JTextField();
+        }
+
+        l[0] = new JLabel("First Name");
+        tf[0].setBounds(300, 50, 300, 35);
+
+        l[1] = new JLabel("Middle Name (optional)");
+        tf[1].setBounds(300, 100, 300, 35);
+
+        l[2] = new JLabel("Last Name");
+        tf[2].setBounds(300, 150, 300, 35);
+
+        l[3] = new JLabel("Gender");
+        rb = new JRadioButton[3];
+        rb[0] = new JRadioButton("M");
+        rb[0].setActionCommand("Male");
+        rb[0].setBounds(300, 200, 40, 35);
+        rb[1] = new JRadioButton("F");
+        rb[1].setActionCommand("Female");
+        rb[1].setBounds(340, 200, 40, 35);
+        rb[2] = new JRadioButton("Other");
+        rb[2].setActionCommand("Other");
+        rb[2].setBounds(380, 200, 80, 35);
+        bg1 = new ButtonGroup();
+        bg1.add(rb[0]);
+        bg1.add(rb[1]);
+        bg1.add(rb[2]);
+
+
+        l[4] = new JLabel("Address");
+        tf[3].setBounds(300, 250, 300, 35);
+
+        l[5] = new JLabel("Phone no. (10 digit)");
+        tf[4].setBounds(300, 300, 300, 35);
+
+        l[6] = new JLabel("E-mail ID");
+        tf[5].setBounds(300, 350, 300, 35);
+
+
+
+        l[7] = new JLabel("User ID");
+        tf[6].setBounds(300, 400, 300, 35);
+
+        l[8] = new JLabel("Old password");
+        pf1 = new JPasswordField();
+        pf1.setBounds(300, 450, 300, 35);
+
+        l[9] = new JLabel("New password");
+        pf2 = new JPasswordField();
+        pf2.setBounds(300, 500, 300, 35);
+
+        l3 = new JLabel("* necessary field");
+        l3.setBounds(300, 700, 300, 40);
+        l3.setForeground(Color.red);
+        l3.setVisible(false);
+        add(l3);
+
+
+        int y = 50;
+        for (JLabel jLabel : l) {
+            jLabel.setBounds(75, y, 300, 40);
+            y += 50;
+        }
+        for (JLabel jLabel : l) {
+            add(jLabel);
+        }
+        for (JTextField jTextField : tf) {
+            add(jTextField);
+        }
+
+        add(rb[0]);
+        add(rb[1]);
+        add(rb[2]);
+        add(pf1);
+        add(pf2);
+
+
+        b1 = new JButton("Update");
+        b1.setActionCommand("update");
+        b1.addActionListener(this);
+        b1.setBounds(87, 760, 150, 30);
+
+        b2 = new JButton("Back");
+        b2.addActionListener(this);
+        b2.setActionCommand("back");
+        b2.setBounds(325, 760, 150, 30);
+
+        b3 = new JButton("Log out");
+        b3.addActionListener((e -> parent.show("login")));
+        b3.setBounds(562, 760, 150, 30);
+        add(b1);
+        add(b2);
+        add(b3);
+        tf[6].setEditable(false);
+        setLayout(null);
+
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/testdb","root","admin");
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("select userid, fname, mname, lname, gender, address, phone, email from admins where userid=\""+username+"\";");
+
+            if (rs.next()) {
+                tf[6].setText(rs.getString(1));             //user id
+                //System.out.println(rs.getString(1));
+                tf[0].setText(rs.getString(2));             //first name
+                tf[1].setText(rs.getString(3));             //middle name
+                tf[2].setText(rs.getString(4));             //last name
+                tf[3].setText(rs.getString(6));             //address
+                tf[4].setText(rs.getString(7));             //phone
+                tf[5].setText(rs.getString(8));             //e-mail
+                old_email_id = rs.getString(8);
+
+                if ("M".equals(rs.getString(5))){           //gender
+                    rb[0].setSelected(true);
+                }
+                else if ("F".equals(rs.getString(5))){
+                    rb[1].setSelected(true);
+                }
+                else{
+                    rb[2].setSelected(true);
+                }
+
+            }
+
+            con.close();
+        }
+        catch(Exception e7){
+            System.out.println(e7);
+        }
+    }
+
+
+    public void actionPerformed(ActionEvent e3) {
+        String cmd = e3.getActionCommand();
+        if ("update".equals(cmd)){
+
+            for (int i = 0; i<10; i++){
+                l2[i].setVisible(false);
+            }
+            l3.setVisible(false);
+
+            boolean flag = true;
+            boolean pwd_flag = true;
+
+            /*password validation*/
+            String pwd1 = new String(pf1.getPassword()) ;
+            String pwd2 = new String(pf2.getPassword());
+            if ((pwd1.equals("") || pwd1==null) && (pwd2.equals("") || pwd2==null)) {
+                pwd_flag = false;
+            }
+            else if ((pwd1.equals("") || pwd1==null) && (!pwd2.equals("") || pwd2!=null)){
+                l2[8].setForeground (Color.red);
+                l2[8].setVisible(true);
+                l3.setText("* necessary field");
+                l3.setVisible(true);
+                flag = false;
+            }
+            else {
+                try{
+                    Class.forName("com.mysql.cj.jdbc.Driver");
+                    Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/testdb","root","admin");
+                    Statement stmt = con.createStatement();
+                    String q2 = "select password from users where userid =\""+username+"\"";
+                    ResultSet rs2 = stmt.executeQuery(q2);
+                    if (rs2.next()){
+                        String pw = rs2.getString(1);
+                        if (!BCrypt.checkpw(pwd1, pw)){
+                            flag=false;
+                            l2[8].setForeground (Color.red);
+                            l2[8].setVisible(true);
+                            l3.setText("* incorrect password");
+                            l3.setVisible(true);
+                        }
+                        else{
+                            l2[8].setForeground (Color.green);
+                            l2[8].setVisible(true);
+                        }
+
+                    }
+                    con.close();
+                }
+                catch (Exception e1){
+                    System.out.println(e1);
+                }
+                if (pwd2.equals("") || pwd2==null){
+                    flag=false;
+                    l2[9].setForeground (Color.red);
+                    l2[9].setVisible(true);
+                    l3.setText("* necessary field");
+                    l3.setVisible(true);
+                }
+                else{
+                    l2[9].setForeground (Color.green);
+                    l2[9].setVisible(true);
+                }
+            }
+
+            /*phone number*/
+            String pno = tf[4].getText();
+            if (pno.equals("") || pno==null){
+                l2[5].setForeground (Color.red);
+                l2[5].setVisible(true);
+                l3.setText("* necessary field");
+                l3.setVisible(true);
+                flag = false;
+            }
+            else if (!pno.matches("\\d+")){
+                l2[5].setForeground (Color.red);
+                l2[5].setVisible(true);
+                l3.setText("* enter digits");
+                l3.setVisible(true);
+                flag = false;
+            }
+            else if (pno.length()!=10){
+                l2[5].setForeground (Color.red);
+                l2[5].setVisible(true);
+                l3.setText("* phone no. should be 10 digit");
+                l3.setVisible(true);
+                flag = false;
+            }
+            else {
+                l2[5].setForeground(Color.green);
+                l2[5].setVisible(true);
+            }
+
+
+
+            /*gender*/
+            String gender = null;
+            boolean flag2 = true;
+            int option =-1;
+            for(int i=0;i<3;i++){
+                if (rb[i].isSelected()){
+                    flag2 = false;
+                    option = i;
+                    break;
+                }
+            }
+            if (flag2){
+                l2[3].setForeground (Color.red);
+                l2[3].setVisible(true);
+                l3.setText("* necessary field");
+                l3.setVisible(true);
+                flag = false;
+            }
+            else{
+                l2[3].setForeground (Color.green);
+                l2[3].setVisible(true);
+                gender = rb[option].getText();
+            }
+
+            /*first name*/
+            String fname2 = tf[0].getText();
+            String fname = "";
+            if (fname2.equals("") || fname2==null){
+                l2[0].setForeground (Color.red);
+                l2[0].setVisible(true);
+                l3.setText("* necessary field");
+                l3.setVisible(true);
+                flag = false;
+            }
+            else if (!fname2.matches("^[a-zA-Z]*$")){
+                l2[0].setForeground (Color.red);
+                l2[0].setVisible(true);
+                l3.setText("* first name should contain only alphabets");
+                l3.setVisible(true);
+                flag = false;
+            }
+            else{
+                l2[0].setForeground (Color.green);
+                l2[0].setVisible(true);
+                String fname3 = fname2.toLowerCase();
+                String s1 = fname3.substring(0, 1).toUpperCase();
+                fname = s1 + fname3.substring(1);
+            }
+
+            /*last name*/
+            String lname2  = tf[2].getText();
+            String lname = "";
+            if (lname2.equals("") || lname2==null){
+                l2[2].setForeground (Color.red);
+                l2[2].setVisible(true);
+                l3.setText("* necessary field");
+                l3.setVisible(true);
+                flag = false;
+            }
+            else if (!lname2.matches("^[a-zA-Z]*$")){
+                l3.setText("* last name should contain only alphabets");
+                l3.setVisible(true);
+                flag = false;
+            }
+            else{
+                l2[2].setForeground (Color.green);
+                l2[2].setVisible(true);
+                String lname3 = lname2.toLowerCase();
+                String s2 = lname3.substring(0, 1).toUpperCase();
+                lname = s2 + lname3.substring(1);
+            }
+
+            /*middle name*/
+            String mname2 = tf[1].getText();
+            String mname = "";
+            if (mname2.equals("") || mname2==null){
+                mname = "";
+            }
+            else{
+                if (!mname2.matches("^[a-zA-Z]*$")){
+                    l2[1].setForeground (Color.red);
+                    l2[1].setVisible(true);
+                    l3.setText("* middle name should contain only alphabets");
+                    l3.setVisible(true);
+                    flag = false;
+                }
+                else{
+                    l2[1].setForeground (Color.green);
+                    l2[1].setVisible(true);
+                    String mname3 = mname2.toLowerCase();
+                    String s3 = mname3.substring(0, 1).toUpperCase();
+                    mname = s3 + mname3.substring(1);
+                }
+            }
+
+
+
+            /*address validation*/
+            String add = tf[3].getText();
+            if (add.equals("") || add==null){
+                l2[4].setForeground (Color.red);
+                l2[4].setVisible(true);
+                l3.setText("* necessary field");
+                l3.setVisible(true);
+                flag = false;
+            }
+            else{
+                l2[4].setForeground (Color.green);
+                l2[4].setVisible(true);
+            }
+
+
+            /*email validation*/
+            String email = tf[5].getText();
+            if (email.equals("") || email==null){
+                l2[6].setForeground (Color.red);
+                l2[6].setVisible(true);
+                l3.setText("* necessary field");
+                l3.setVisible(true);
+                flag = false;
+            }
+            else if (!email.matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$")){
+                l2[6].setForeground (Color.red);
+                l2[6].setVisible(true);
+                l3.setText("* invalid e-mail");
+                l3.setVisible(true);
+                flag = false;
+            }
+            else{
+                l2[6].setForeground (Color.green);
+                l2[6].setVisible(true);
+            }
+            if (!email.equals(old_email_id)){
+                if (flag){
+                    Random rand = new Random();
+                    int rand_int1 = rand.nextInt(1000000);
+                    if (rand_int1 < 100000){
+                        rand_int1 += 100000;
+                    }
+                    String msg = "Your six digit verification code is " + rand_int1 + ", don't share your verification code with anyone";
+                    String sub = "Verify your e-mail address";
+                    automailer verify = new automailer(email);
+                    verify.send_user(sub,msg,false,null);
+
+                    JPanel panel = new JPanel();
+                    JLabel label = new JLabel("Enter the 6 digit verification code sent to your e-mail id : ");
+                    JPasswordField pass = new JPasswordField(10);
+                    panel.add(label);
+                    panel.add(pass);
+                    String[] options = new String[]{"OK"};
+                    JOptionPane.showOptionDialog(null, panel, "E-mail verification",
+                            JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE,
+                            null, options, options[0]);
+
+                    int result = Integer.parseInt(String.valueOf(pass.getPassword()));
+
+                    if (result!=rand_int1){
+                        JOptionPane.showMessageDialog(null,"Try again","Verification failed",JOptionPane.INFORMATION_MESSAGE);
+                        l2[6].setForeground (Color.red);
+                        l2[6].setVisible(true);
+                        l3.setText("* e-mail not verified");
+                        l3.setVisible(true);
+                        flag=false;
+                    }
+                    else{
+                        JOptionPane.showMessageDialog(null,"Your e-mail address has been successfully updates","Success",JOptionPane.INFORMATION_MESSAGE);
+                    }
+                }
+            }
+
+            if (flag){
+                try{
+                    Class.forName("com.mysql.cj.jdbc.Driver");
+                    Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/testdb","root","admin");
+                    Statement stmt = con.createStatement();
+                    if (pwd_flag){
+                        String pow = BCrypt.hashpw(pwd2, BCrypt.gensalt());
+                        stmt.executeUpdate("UPDATE admins SET fname =\""+fname +"\", mname =\""+mname +"\",lname =\""+lname +"\",gender =\""+gender+"\",address=\""+add+"\",phone=\""+ pno+"\",email= \""+email+"\",password=\""+pow+"\" WHERE userid = \"" + username + "\";");
+                    }
+                    else{
+                        stmt.executeUpdate("UPDATE admins SET fname =\""+fname +"\", mname =\""+mname +"\",lname =\""+lname +"\",gender =\""+gender+"\",address=\""+add+"\",phone=\""+ pno+"\",email= \""+email+"\" WHERE userid = \"" + username + "\";");
+                    }
+                    JOptionPane.showMessageDialog(null,"Successfully updated");
+                    pointer.setTitle("Welcome");
+                    con.close();
+                    pf1.setText("");
+                    pf2.setText("");
+                    pointer.show("welcome_admin");
+                }
+                catch(Exception e2){
+                    System.out.println(e2);
+                }
+            }
+
+        }
+        else if (cmd.equals("back")){
+            pointer.setTitle("Welcome");
+            pointer.show("welcome_admin");
+        }
+
+    }
+}
---
+++ src/welcome.java
@@ -0,0 +1,93 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.lang.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class welcome extends JPanel implements ActionListener {
+    private  JButton b1, b2, b3;
+    JLabel l1, l2, l3, l4;
+    String s1, s2, s3, s4;
+    private masterwindow pointer;
+    public welcome(masterwindow parent, String uid) {
+        pointer = parent;
+        pointer.setTitle("Welcome");
+        l1 = new JLabel();
+        l2 = new JLabel();
+        l3 = new JLabel();
+        l4 = new JLabel();
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/testdb","root","admin");
+            Statement stmt = con.createStatement();
+            ResultSet rs=stmt.executeQuery("select fname, mname, lname from users where userid=\""+uid+"\"");
+
+            if (rs.next()) {
+                s1 = rs.getString(1);
+                s2 = rs.getString(2);
+                s3 = rs.getString(3);
+            }
+            con.close();
+        }
+        catch(Exception e1){
+            System.out.println(e1);
+        }
+        l4.setText("Welcome,");
+        l4.setBounds(50,50,400,70);
+        l4.setFont(l4.getFont().deriveFont(70f));
+        add(l4);
+        l1.setText(s1);
+        l1.setBounds(50,120,400,70);
+        l1.setFont(l1.getFont().deriveFont(70f));
+        add(l1);
+        int yp = 190;
+        if ("".equals(s2) || s2.equals(null)){
+        }
+        else{
+            l2.setText(s2);
+            l2.setBounds(50,yp,400,70);
+            l2.setFont(l2.getFont().deriveFont(70f));
+            add(l2);
+            yp+=70;
+        }
+        l3.setText(s3);
+        l3.setBounds(50,yp,400,70);
+        l3.setFont(l3.getFont().deriveFont(70f));
+        add(l3);
+
+        b1 = new JButton("Update information");
+        b1.addActionListener(this);
+        b1.setActionCommand("update");
+        b1.setBounds(87,760,150,30);
+
+        b2 = new JButton("Calculate tax");
+        b2.addActionListener(this);
+        b2.setActionCommand("calculator");
+        b2.setBounds(325,760,150,30);
+
+        b3 = new JButton("Log out");
+        b3.addActionListener((e -> parent.show("login")));
+        b3.setBounds(562,760,150,30);
+
+        add(b1);
+        add(b2);
+        add(b3);
+        setLayout(null);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String cmd = e.getActionCommand();
+        if (cmd.equals("update")){
+            pointer.setTitle("Update");
+            pointer.show("update");
+        }
+        else if (cmd.equals("calculator")){
+            pointer.setTitle("Calculate Tax");
+            pointer.show("calculator");
+        }
+    }
+}
